79397d5cca01ab578c8628401c4d275f9166580e
Ayush Mehta <mehtaayush144@gmail.com>
2025-05-17 19:17:40 -0700

    Added Mocha Test file and folders for testing purposes

---
+++ Mocha/README.md
@@ -0,0 +1,14 @@
+EchoJournal Mocha Test
+Sample Mocha test
+
+In this folder, you will find the various TDD test cases using mocha:
+* Test cases are located in the test directory
+* Initialize the pre-requisite modules, including mocha, by running "npm install"
+* Run test case using the following command "runTestFile.cmd"
+
+For more details on how to configure and use mocha go to:
+http://mochajs.org/
+
+For more details on how to configure chai and chai-http go to:
+http://chaijs.com/plugins/chai-http
+http://chaijs.com/api/bdd/
---
+++ Mocha/test/AppTestFile.js
@@ -0,0 +1,113 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+const BASE_URL = 'http://localhost:8080';
+
+describe('Journal Entry API Tests', function () {
+    let testUserId = 1;
+    let testEntryId;
+
+    before(function (done) {
+        chai.request(BASE_URL)
+            .post('/app/journal/')
+            .send({
+                userId: testUserId,
+                content: 'I did this Test journal entry',
+                feelings: ['happy', 'excited'],
+                date: new Date().toISOString()
+            })
+            .end(function (err, res) {
+                expect(res).to.have.status(201);
+                expect(res.body).to.have.property('data');
+                testEntryId = res.body.data.id;
+                done();
+            });
+    });
+
+    describe('GET /app/journal/:id/:userId', function () {
+        it('Should return a journal entry with expected properties for entry id', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/${testEntryId}/${testUserId}`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('success', true);
+                    expect(res.body).to.have.property('data');
+                    expect(res.body.data).to.include.keys('_id', 'id', 'userId', 'content', 'feelings', 'date', 'createdAt', 'updatedAt', '__v');
+                    expect(res.body.data.content).to.equal('I did this Test journal entry');
+                    expect(res.body.data.feelings).to.deep.equal(['happy', 'excited']);
+                    expect(res.body.data.userId).to.equal(testUserId);
+                    done();
+                });
+        });
+
+        it('Should return 404 for non-existent id/userId', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/000000000000000000000000/${testUserId}`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res.body).to.have.property('success', false);
+                    done();
+                });
+        });
+
+        it('Should return 404 for invalid id format', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/123456678/${testUserId}`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res.body).to.have.property('success', false);
+                    expect(res.body).to.have.property('message', 'Journal entry not found');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /app/journal/all/:userId', function () {
+        it('Should return an array of journal entries for valid userId', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/all/${testUserId}`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('success', true);
+                    expect(res.body).to.have.property('entries');
+                    expect(res.body.entries).to.be.an('array');
+                    if (res.body.entries.length > 0) {
+                        expect(res.body.entries[0]).to.include.keys(
+                            '_id', 'id', 'userId', 'content', 'feelings', 'date', 'createdAt', 'updatedAt', '__v'
+                        );
+                    }
+                    done();
+                });
+        });
+
+
+        it('Should support pagination with page and limit query', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/all/${testUserId}?page=1&limit=1`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('success', true);
+                    expect(res.body).to.have.property('entries');
+                    expect(res.body.entries).to.be.an('array');
+                    expect(res.body.entries.length).to.be.at.most(1);
+                    done();
+                });
+        });
+
+        it('Should return empty array for userId with no entries', function (done) {
+            chai.request(BASE_URL)
+                .get(`/app/journal/all/999999`)
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('success', true);
+                    expect(res.body).to.have.property('entries');
+                    expect(res.body.entries).to.be.an('array');
+                    expect(res.body.entries.length).to.equal(0);
+                    done();
+                });
+        });
+    });
+});

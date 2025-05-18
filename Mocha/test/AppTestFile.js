88fcb65d6d8aec480670082da896afdd167f7a5b
Rupeshwar Rao <raorupesh24@gmail.com>
2025-05-17 21:05:44 -0700

    Update Mocha test file as per new route changes

--- Mocha/test/AppTestFile.js
+++ Mocha/test/AppTestFile.js
@@ -12,7 +12,7 @@ describe('Journal Entry API Tests', function () {
 
     before(function (done) {
         chai.request(BASE_URL)
-            .post('/app/journal/')
+            .post('/api/v1/journal/')
             .send({
                 userId: testUserId,
                 content: 'I did this Test journal entry',
@@ -27,10 +27,10 @@ describe('Journal Entry API Tests', function () {
             });
     });
 
-    describe('GET /app/journal/:id/:userId', function () {
+    describe('GET /api/v1/journal/:id', function () {
         it('Should return a journal entry with expected properties for entry id', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/${testEntryId}/${testUserId}`)
+                .get(`/api/v1/journal/${testEntryId}`)
                 .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.have.property('success', true);
@@ -43,9 +43,9 @@ describe('Journal Entry API Tests', function () {
                 });
         });
 
-        it('Should return 404 for non-existent id/userId', function (done) {
+        it('Should return 404 for non-existent id', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/000000000000000000000000/${testUserId}`)
+                .get(`/api/v1/journal/000000000000000000000000`)
                 .end(function (err, res) {
                     expect(res).to.have.status(404);
                     expect(res.body).to.have.property('success', false);
@@ -55,7 +55,7 @@ describe('Journal Entry API Tests', function () {
 
         it('Should return 404 for invalid id format', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/123456678/${testUserId}`)
+                .get(`/api/v1/journal/123456678`)
                 .end(function (err, res) {
                     expect(res).to.have.status(404);
                     expect(res.body).to.have.property('success', false);
@@ -65,10 +65,10 @@ describe('Journal Entry API Tests', function () {
         });
     });
 
-    describe('GET /app/journal/all/:userId', function () {
+    describe('GET /api/v1/journal/all', function () {
         it('Should return an array of journal entries for valid userId', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/all/${testUserId}`)
+                .get(`/api/v1/journal/all?userId=${testUserId}`)
                 .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.have.property('success', true);
@@ -83,10 +83,9 @@ describe('Journal Entry API Tests', function () {
                 });
         });
 
-
         it('Should support pagination with page and limit query', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/all/${testUserId}?page=1&limit=1`)
+                .get(`/api/v1/journal/all?userId=${testUserId}&page=1&limit=1`)
                 .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.have.property('success', true);
@@ -99,7 +98,7 @@ describe('Journal Entry API Tests', function () {
 
         it('Should return empty array for userId with no entries', function (done) {
             chai.request(BASE_URL)
-                .get(`/app/journal/all/999999`)
+                .get(`/api/v1/journal/all?userId=999999`)
                 .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.have.property('success', true);
